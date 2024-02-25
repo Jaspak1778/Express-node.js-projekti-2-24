921377e07f04bd33caccbcc1b35387397ab26a18
Jani Pekkala <pekkala.jani@hotmail.fi>
2024-02-25 14:50:44 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const tiedot = require('./tiedot.json');
+const hinnat = require('./hinnat.json');
+const PORT = process.env.PORT || 3000;
+
+app.get('/api/tiedot', (req, res) => {
+    res.json(tiedot);
+});
+
+app.get('/api/hinnat', (req, res) => {
+    res.json(hinnat);
+});
+
+const polku = path.join(__dirname, '/public');
+app.use(express.static(polku));
+
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`);
+  });
